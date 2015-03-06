57422cdb2f3978a1ebe5fa4780f3d7c0f333089b
Alejandro Norberto Pasquini <Ale9Hack@gmail.com>
2015-03-06 18:47:33 -0300

    controller and model

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,9 +1,9 @@
 // chat controller for chat model require by sockets
-var chatModel= require('../models/chats')();
+var chat= require('../models/chats')();
 exports.databaseSave = function (username,msg){
-var incomingChat = new chatModel({ username: username,msg:msg, });
+var incomingChat = new chat({ username: username,msg:msg, });
  	incomingChat.save();
-}
+};
 
 /*chatMongo.find({},{},{limit:6, sort:{date:-1 }}, function(err,chats){
 	if(err){
