24bd9e622329ff24350c1692b66ca789d70e10b6
Alejandro Norberto Pasquini <Ale9Hack@gmail.com>
2015-03-13 06:54:37 -0300

    update users mvc

--- models/users.js
+++ models/users.js
@@ -1,15 +1,49 @@
 'use strict'
 var db= require('../config/mongoose')();
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR=10;
 
 module.exports = function (){
 var Schema = db.mongoose.Schema;
 var userSchema = new Schema({ 
-	username: {type:String,unique:true,required:'El usuario es requerido'}, 
-	password: {type: String, required:'La contraseña es requerida'},
-	token: {type:String, unique:true},
-	date: { type: Date, default: Date.now}
+		username: {type:String,unique:true,required:'El usuario es requerido'},
+		email: {type:String,unique:true,required:"El email es requerido"}, 
+		password: {type: String, required:'La contraseña es requerida'},
+		admin: {type:Boolean},
+		token: {type:String},
+		date: { type: Date, default: Date.now}
 
  }, {/*opt*/});
+
+
+
+// Bcrypt middleware
+userSchema.pre('save', function(next) {
+	var user = this;
+
+	if(!user.isModified('password')) return next();
+
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+		if(err) return next(err);
+
+		bcrypt.hash(user.password, salt, function(err, hash) {
+			if(err) return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+// Password verification
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+	bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+		if(err) return cb(err);
+		cb(null, isMatch);
+	});
+};
+
+
+
 
  return  db.connection.model('User', userSchema);
 
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,25 @@
 var express = require('express');
+var passport = require('passport');
+var pass = require('../config/passport')
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 
 var user;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', pass.ensureAuthenticated, function(req, res, next) {
 	
-	user = req.session;
-	res.render('user', {username:user.name});
+		res.render('user', {username:req.user});
  });
+
+router.get('/login', function(req, res, next) {
+		res.cookie('token', jwt.sign({'user':'test'}, 'omg' ,{ expiresInMinutes: 60*5 })) //for testing
+		res.render('sign-in', {});
+ });
+
+
+
+
+
 
 
 router.post('/login', function(req,res,next){
